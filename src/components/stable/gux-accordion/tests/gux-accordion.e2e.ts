1a95afda0aa963210b6ad3008c71bfc0c2267c84
Conor Darcy <conor.darcy@genesys.com>
2022-07-06 12:10:52 +0100

    test(accordion): updating snapshots

--- src/components/stable/gux-accordion/tests/gux-accordion.e2e.ts
+++ src/components/stable/gux-accordion/tests/gux-accordion.e2e.ts
@@ -1,24 +1,7 @@
 import { newSparkE2EPage, a11yCheck } from '../../../../../tests/e2eTestUtils';
+import { html } from './gux-accordion.markup';
 
 describe('gux-accordion', () => {
-  const html = `
-  <gux-accordion lang="en" text="Primary" accent="primary">
-    <gux-accordion>
-    <gux-accordion-section>
-      <h2 slot="header">First Section</h2>
-      <div slot="content">Sample Content</div>
-    </gux-accordion-section>
-    <gux-accordion-section>
-      <h2 slot="header">Second Section</h2>
-      <div slot="content">Sample Content</div>
-    </gux-accordion-section>
-    <gux-accordion-section>
-      <h2 slot="header">Third Section</h2>
-      <div slot="content">Sample Content</div>
-    </gux-accordion-section>
-  </gux-accordion>
-  `;
-
   it('renders', async () => {
     const page = await newSparkE2EPage({ html });
     const element = await page.find('gux-accordion');
--- src/components/stable/gux-accordion/tests/gux-accordion.spec.ts
+++ src/components/stable/gux-accordion/tests/gux-accordion.spec.ts
@@ -1,24 +1,46 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { GuxAccordion } from '../gux-accordion';
 import { GuxAccordionSection } from '../gux-accordion-section/gux-accordion-section';
+import { html } from './gux-accordion.markup';
 
 const components = [GuxAccordion, GuxAccordionSection];
-const html = `
-<gux-accordion>
-  <gux-accordion-section>
-    <h2 slot="header">First Section</h2>
-    <div slot="content">Sample Content</div>
-  </gux-accordion-section>
-  <gux-accordion-section>
-    <h2 slot="header">Second Section</h2>
-    <div slot="content">Sample Content</div>
-  </gux-accordion-section>
-  <gux-accordion-section>
-    <h2 slot="header">Third Section</h2>
-    <div slot="content">Sample Content</div>
-  </gux-accordion-section>
-</gux-accordion>
-`;
+// const html = `
+// <gux-accordion>
+//   <gux-accordion-section>
+//     <h2 slot="header">First Section</h2>
+//     <div slot="content">Sample Content</div>
+//   </gux-accordion-section>
+//   <gux-accordion-section arrow-position="beside-text">
+//     <h2 slot="header">Second Section</h2>
+//     <div slot="content">Sample Content</div>
+//   </gux-accordion-section>
+//   <gux-accordion-section disabled>
+//     <h2 slot="header">Third Section</h2>
+//     <h3 slot="subheading">This is the subheading</h3>
+//     <div slot="content">Sample Content</div>
+//   </gux-accordion-section>
+//   <gux-accordion-section>
+//     <h2 slot="header">Fourth Section</h2>
+//     <h3 slot="subheading">This is the subheading</h3>
+//     <gux-icon
+//       slot="icon"
+//       icon-name="add-user"
+//       screenreader-text="add John Smith to contact list"
+//     ></gux-icon>
+//     <div slot="content">Sample Content</div>
+//   </gux-accordion-section>
+//   <gux-accordion-section reverse-headings="true">
+//     <h2 slot="header">Fourth Section</h2>
+//     <h3 slot="subheading">This is the subheading</h3>
+//     <gux-icon
+//       slot="icon"
+//       icon-name="add-user"
+//       screenreader-text="add John Smith to contact list"
+//     ></gux-icon>
+//     <div slot="content">Sample Content</div>
+//   </gux-accordion-section>
+// </gux-accordion>
+// `;
 const language = 'en';
 
 describe('gux-accordion', () => {
